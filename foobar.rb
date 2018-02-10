65e38caf04702652a48149899927b66d266f28f8
Clement Ng <clementng@berkeley.edu>
2018-02-10 03:19:21 -0800

    Finished hw1

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,6 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a.map{ |e| Integer(e) }.map{ |e| e + 2 }.select{ |e| e.even? }.uniq.select{ |e| e < 10}.inject(0){|sum,x| sum + x}
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,17 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  (a + b) ** 2
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.sort!.map!{|item| item + 1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  first_name + ' ' + last_name
 end
 
 def blockin_time(a)
@@ -49,4 +50,8 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  # word.chars.each do |element|
+  #   puts values[element.to_sym]
+  # end
+  word.chars.map { |elem| values[elem.to_sym]}.inject(0){|sum,x| sum + x}
 end
